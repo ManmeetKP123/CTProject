6bf4f47f6486260ea01511084d5510d048d82d1a
ManmeetKP123 <purewalmanmeet93@gmail.com>
2022-06-24 10:13:27 -0700

    finished training code

---
+++ nn_modules/config_file.py
@@ -0,0 +1,7 @@
+
+class config_file():
+    
+    batch_size = 2
+    learning_rate = 0.001
+    epochs = 30
+    momentum = 0.9
--- nn_modules/training.py
+++ nn_modules/training.py
@@ -1,22 +1,52 @@
+from distutils.command.config import config
 import torch 
 from torch import nn 
 import torch.nn.functional as F 
 from torch.utils.data import Dataset, DataLoader 
-import unet3d
+from unet3d import UNet3D
+from dataloaders import CTDataset
+from config_file import config_file
 
 #define the dice loss 
-class DiceLoss(nn.Module):
-    def __init__(self, weight=None, size_average = True) -> None:
-        super(DiceLoss, self).__init__()
-    
-    def forward(self, y_pred, y_true, smooth = 1):
+device = torch.device("cuda")
+def dice_loss(self, y_pred, y_true, smooth = 1):
 
-        #difference between flatten vs view??
+    #difference between flatten vs view??
 
-        y_pred = y_pred.view(-1)
-        y_true = y_true.view(-1)
+    y_pred = y_pred.view(-1)
+    y_true = y_true.view(-1)
 
-        intersection = (y_true * y_pred).sum()
-        dice = (2 * intersection  + smooth)/(y_true.sum() + y_pred.sum() + smooth)
+    intersection = (y_true * y_pred).sum()
+    dice = (2 * intersection  + smooth)/(y_true.sum() + y_pred.sum() + smooth)
 
-        return 1 - dice()
+    return 1 - dice()
+
+
+train_loader = DataLoader(CTDataset, batch_size = config_file.batch_size, shuffle = True)
+
+model = UNet3D()
+model.to(device)
+criterion = dice_loss
+
+#Paper used optimizer 
+optimizer = torch.optim.Adam(model.parameters(), config_file.learning_rate)
+
+# Print model's state_dict
+print("Model's state_dict:")
+for param_tensor in model.state_dict():
+    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+
+
+
+# for epoch in range(config_file.epochs):
+#     scheduler.step(epoch)
+#     model.train()
+
+#     for batch_idx, (x, y) in enumerate(train_loader):
+#         x, y = x.float().to(device), y.float().to(device)
+#         pred = model(x)
+#         loss = criterion(pred, y)
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+
